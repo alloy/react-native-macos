69a88d43b8230a7c75b586da601355aa0d6a0a25
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2020-10-02 15:16:29 -0700

    Use the correct podfile

--- scripts/react_native_pods.rb
+++ scripts/react_native_pods.rb
@@ -55,8 +55,6 @@ def use_react_native! (options={})
   # TODO(macOS GH#214)
   pod 'boost-for-react-native', :podspec => "#{prefix}/third-party-podspecs/boost-for-react-native.podspec"
 
-  pod 'React-TurboModuleCxx-MacOS', :podspec => "#{prefix}/ReactTurboModuleCxx/MacOS.podspec"
-
   if fabric_enabled
     pod 'React-Fabric', :path => "#{prefix}/ReactCommon"
     pod 'React-graphics', :path => "#{prefix}/ReactCommon/fabric/graphics"
